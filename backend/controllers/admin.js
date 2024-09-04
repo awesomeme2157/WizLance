ddae5fe40fa01a47b4d8bbf54537502cbce8251e
awesomeme2157 <rajmhetar007@gmail.com>
2024-09-04 17:05:31 +0530

    Fixed controllers

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -9,187 +9,184 @@ const subcategory = require("../models/subcategory");
 
 // user api
 exports.displayUsers = async (req, res) => {
-    const users = await User.find({}, { password: 0 });
-    res.status(200).json({
-        users,
-    });
+  const users = await User.find({}, { password: 0 });
+  res.status(200).json({
+    users,
+  });
 };
 
 exports.deleteGig = async (req, res) => {
-    const gigId = req.body.gigId;
-    await Gig.findByIdAndRemove(gigId);
-    res.status(200).json({
-        message: "Gig deleted successfully",
-    });
+  const gigId = req.body.gigId;
+  await Gig.findByIdAndRemove(gigId);
+  res.status(200).json({
+    message: "Gig deleted successfully",
+  });
 };
 
 exports.searchGigs = async (req, res) => {
-    const searchQuery = req.body.searchQuery;
-    const gigs = await Gig.find({
-        name: { $regex: searchQuery, $options: "i" },
-    });
-    res.status(200).json({
-        gigs,
-    });
+  const searchQuery = req.body.searchQuery;
+  const gigs = await Gig.find({
+    name: { $regex: searchQuery, $options: "i" },
+  });
+  res.status(200).json({
+    gigs,
+  });
 };
 
 exports.displayCategories = async (req, res) => {
-    try {
-        const categories = await Category.find({});
-        console.log("Fetched categories:", categories);
+  try {
+    const categories = await Category.find({});
+    console.log("Fetched categories:", categories);
 
-        const key = req.originalUrl;
-        // Seting the data to Redis
-        await client.set(key, JSON.stringify(categories));
+    const key = req.originalUrl;
+    // Seting the data to Redis
+    await client.set(key, JSON.stringify(categories));
 
-        console.log(key, "\nFetching from Database");
+    console.log(key, "\nFetching from Database");
 
-        res.status(200).json({
-            categories,
-        });
-    } catch (error) {
-        console.error("Error fetching categories:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    res.status(200).json({
+      categories,
+    });
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 exports.displaySubCategories = async (req, res, next) => {
-    try {
-        const subCategories = await SubCategories.find({});
+  try {
+    const subCategories = await SubCategories.find({});
 
-        const key = req.originalUrl;
-        // Seting the data to Redis
-        await client.set(key, JSON.stringify(subCategories));
-        console.log(key, "\nFetching from Database");
+    const key = req.originalUrl;
+    // Seting the data to Redis
+    await client.set(key, JSON.stringify(subCategories));
+    console.log(key, "\nFetching from Database");
 
-        if (!subCategories) {
-            const err = new Error("No subcategories found");
-            err.statusCode = 404;
-            throw err;
-        }
-
-        const subcategories = await Promise.all(
-            subCategories.map(async (subCategory) => {
-                const category = await Category.findById(
-                    subCategory.categoryId
-                );
-                return {
-                    ...subCategory.toObject(),
-                    category: category,
-                };
-            })
-        );
-        console.log(subcategories);
-        res.status(200).json({
-            subcategories,
-        });
-    } catch (err) {
-        if (!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
+    if (!subCategories) {
+      const err = new Error("No subcategories found");
+      err.statusCode = 404;
+      throw err;
     }
+
+    const subcategories = await Promise.all(
+      subCategories.map(async (subCategory) => {
+        const category = await Category.findById(subCategory.categoryId);
+        return {
+          ...subCategory.toObject(),
+          category: category,
+        };
+      })
+    );
+    console.log(subcategories);
+    res.status(200).json({
+      subcategories,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.displayGigs = async (req, res) => {
-    const type = req.body.type;
-    const id = req.body.id;
+  const type = req.body.type;
+  const id = req.body.id;
 
- 
-    let gigs;
-    switch (type) {
-        case "subcategory":
-            gigs = await Gig.find({ subcategoryId: id });
-            break;
-        case "user":
-            gigs = await Gig.find({ freelancer: id });
-            break;
-        default:
-            gigs = await Gig.find({});
-    }
+  let gigs;
+  switch (type) {
+    case "subcategory":
+      gigs = await Gig.find({ subcategoryId: id });
+      break;
+    case "user":
+      gigs = await Gig.find({ freelancer: id });
+      break;
+    default:
+      gigs = await Gig.find({});
+  }
 
-    res.status(200).json({
-        gigs,
-    });
+  res.status(200).json({
+    gigs,
+  });
 };
 
 exports.allGigs = async (req, res) => {
-    const gigs = await Gig.find({})
-        .populate({
-            path: "subCategoryId",
-            select: "name",
-        })
-        .populate({
-            path: "freelancer",
-            select: "firstName",
-        });
-    res.status(200).json({
-        gigs,
+  const gigs = await Gig.find({})
+    .populate({
+      path: "subCategoryId",
+      select: "name",
+    })
+    .populate({
+      path: "freelancer",
+      select: "firstName",
     });
+  res.status(200).json({
+    gigs,
+  });
 };
 
 exports.deleteFromUser = (req, res) => {
-    const requiredMail = req.body.deleteFromEmail;
-    User.findOneAndRemove({ email: requiredMail }).then((user) => {
-        if (user?.isFreelancer) {
-            Gig.deleteMany({ freelancerEmail: user.email }).then();
-        }
-    });
+  const requiredMail = req.body.deleteFromEmail;
+  User.findOneAndRemove({ email: requiredMail }).then((user) => {
+    if (user?.isFreelancer) {
+      Gig.deleteMany({ freelancerEmail: user.email }).then();
+    }
+  });
 };
 
 exports.addCategory = (req, res) => {
-    const newCategory = req.body.addCategory;
-    const image = req.body.addCategoryImage;
-    const catObj = new Category({
-        name: newCategory,
-        imageUrl: image,
-    });
-    catObj.save();
+  const newCategory = req.body.addCategory;
+  const image = req.body.addCategoryImage;
+  const catObj = new Category({
+    name: newCategory,
+    imageUrl: image,
+  });
+  catObj.save();
 
-    setTimeout(() => {
-        return res.redirect("/admin-categories");
-    }, 50);
+  setTimeout(() => {
+    return res.redirect("/admin-categories");
+  }, 50);
 };
 
 exports.addSubCategory = (req, res) => {
-    const requiredCategory = req.body.selectCategory;
-    const image = req.body.addSubCategoryImage;
+  const requiredCategory = req.body.selectCategory;
+  const image = req.body.addSubCategoryImage;
 
-    const newSubCategory = {
-        name: req.body.addSubCategory,
-        imageUrl: image,
-    };
+  const newSubCategory = {
+    name: req.body.addSubCategory,
+    imageUrl: image,
+  };
 
-    Category.findOneAndUpdate(
-        { name: requiredCategory.toString() },
-        { $push: { subCategories: newSubCategory } }
-    ).then(() => {
-        return res.redirect("/admin-categories");
-    });
+  Category.findOneAndUpdate(
+    { name: requiredCategory.toString() },
+    { $push: { subCategories: newSubCategory } }
+  ).then(() => {
+    return res.redirect("/admin-categories");
+  });
 };
 
 exports.deleteCategory = (req, res) => {
-    const categoryChosen = req.body.selectedCategory;
+  const categoryChosen = req.body.selectedCategory;
 
-    Category.deleteOne({ name: categoryChosen }).then(() => {
-        return res.redirect("/admin-categories");
-    });
+  Category.deleteOne({ name: categoryChosen }).then(() => {
+    return res.redirect("/admin-categories");
+  });
 };
 
 exports.deleteSubCategory = (req, res) => {
-    const categoryChosen = req.body.selectedCategory;
-    const subCategoryChosen = req.body.deleteSubCategory;
+  const categoryChosen = req.body.selectedCategory;
+  const subCategoryChosen = req.body.deleteSubCategory;
 
-    Category.findOneAndUpdate(
-        { name: categoryChosen.toString() },
-        { $pull: { subCategories: { name: subCategoryChosen } } }
-    ).then(() => {
-        return res.redirect("/admin-categories");
-    });
+  Category.findOneAndUpdate(
+    { name: categoryChosen.toString() },
+    { $pull: { subCategories: { name: subCategoryChosen } } }
+  ).then(() => {
+    return res.redirect("/admin-categories");
+  });
 };
 
 exports.getDashboard = (req, res, next) => {
-    res.render("pages/admin-dashboard", { admin: req.session.user });
+  res.render("pages/admin-dashboard", { admin: req.session.user });
 };
 
 // exports.contactAdmin = (req, res) => {
@@ -208,40 +205,40 @@ exports.getDashboard = (req, res, next) => {
 // };
 
 exports.postMail = (req, res, next) => {
-    const subject = req.body.mailSubject;
-    const message = req.body.mailMessage;
-    console.log(subject, message);
+  const subject = req.body.mailSubject;
+  const message = req.body.mailMessage;
+  console.log(subject, message);
 
-    const transport = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "wiz.project13@gmail.com",
-            pass: "cxtdpaqbbnacvtft",
-        },
-    });
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "wiz.project13@gmail.com",
+      pass: "cxtdpaqbbnacvtft",
+    },
+  });
 
-    User.find({}, { email: 1 })
-        .then((emails) => {
-            return emails.forEach((e) => {
-                const mailOptions = {
-                    to: e.email,
-                    form: "wiz.project13@gmail.com",
-                    subject: subject,
-                    html: message,
-                };
+  User.find({}, { email: 1 })
+    .then((emails) => {
+      return emails.forEach((e) => {
+        const mailOptions = {
+          to: e.email,
+          form: "wiz.project13@gmail.com",
+          subject: subject,
+          html: message,
+        };
 
-                transport.sendMail(mailOptions, (error, info) => {
-                    if (error) {
-                        return res.status(500).send(error.toString());
-                    }
-                    res.status(200).send("Email sent: " + info.response);
-                });
-            });
-        })
-        .catch((err) => {
-            console.log(err);
+        transport.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            return res.status(500).send(error.toString());
+          }
+          res.status(200).send("Email sent: " + info.response);
         });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 exports.getMailPage = (req, res, next) => {
-    res.render("pages/admin-sendMail");
+  res.render("pages/admin-sendMail");
 };
--- backend/controllers/freelancer.js
+++ backend/controllers/freelancer.js
@@ -11,43 +11,45 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
 });
 
-
 // addGig Updated
 exports.addGigs = async (req, res) => {
   try {
-    const { email, gig, category, subCategory, description, price ,image } = req.body;
+    const { email, gig, category, subCategory, description, price, image } =
+      req.body;
     const imageUrl = req.file.path;
     console.log(req.file);
     console.log(subCategory);
 
     // cloudinary setup
-    
+
     let date = new Date();
-    let dateStr = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
-    
+    let dateStr = `${date.getDate()}-${
+      date.getMonth() + 1
+    }-${date.getFullYear()}`;
+
     const uploadedResponse = await cloudinary.uploader.upload(imageUrl, {
-      folder: 'wizlance-gig',
-      public_id: `${dateStr}--${gig}--${subCategory}`
+      folder: "wizlance-gig",
+      public_id: `${dateStr}--${gig}--${subCategory}`,
     });
 
     const cloudinaryImageUrl = uploadedResponse.secure_url;
 
-      // Find the user's id based on their email
-      const user = await User.findOne({ email });
-      console.log(user, 'user') ;
-      
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
+    // Find the user's id based on their email
+    const user = await User.findOne({ email });
+    console.log(user, "user");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
     // Find the SubCategory ID based on the SubCategory name
-   // Find the SubCategory document by its name
-   const subCategoryObj = await SubCategory.findOne({ name: subCategory });
+    // Find the SubCategory document by its name
+    const subCategoryObj = await SubCategory.findOne({ name: subCategory });
 
-   if (!subCategory) {
-     return res.status(404).json({ message: "SubCategory not found" });
-   }
-    
+    if (!subCategory) {
+      return res.status(404).json({ message: "SubCategory not found" });
+    }
+
     const gigData = new Gig({
       name: gig,
       price: price,
@@ -55,73 +57,73 @@ exports.addGigs = async (req, res) => {
       imageUrl: cloudinaryImageUrl,
       description: description,
       subCategoryId: subCategoryObj._id, // Assign the SubCategory ID
-      });
+    });
 
-       await gigData.save();
+    await gigData.save();
 
-        return res.status(200).json({
-            message: "Gig added successfully",
-        });
-    } catch (error) {
-        console.error("Error adding gig:", error);
-        return res.status(500).json({
-            message: "An error occurred while adding the gig",
-        });
-    }
+    return res.status(200).json({
+      message: "Gig added successfully",
+    });
+  } catch (error) {
+    console.error("Error adding gig:", error);
+    return res.status(500).json({
+      message: "An error occurred while adding the gig",
+    });
+  }
 };
 
 exports.addSkill = async (req, res) => {
-    const name = req.body.name;
-    const skill = new Skill({
-        name,
-    });
+  const name = req.body.name;
+  const skill = new Skill({
+    name,
+  });
 
-    try {
-        await skill.save();
-        res.status(201).json({ skill });
-    } catch (error) {
-        console.error("Error adding skill:", error);
-        return res.status(500).json({
-            message: "An error occurred while adding the gig",
-        });
-    }
+  try {
+    await skill.save();
+    res.status(201).json({ skill });
+  } catch (error) {
+    console.error("Error adding skill:", error);
+    return res.status(500).json({
+      message: "An error occurred while adding the gig",
+    });
+  }
 };
 
 exports.getSkills = async (req, res) => {
-    try {
-        const skills = await Skill.find();
-        res.status(200).json({ skills });
-    } catch (error) {
-        console.error("Error getting skills:", error);
-        return res.status(500).json({
-            message: "An error occurred while getting the skills",
-        });
-    }
+  try {
+    const skills = await Skill.find();
+    res.status(200).json({ skills });
+  } catch (error) {
+    console.error("Error getting skills:", error);
+    return res.status(500).json({
+      message: "An error occurred while getting the skills",
+    });
+  }
 };
 
 exports.becomeFreelancer = async (req, res) => {
-    const userId = req.body.userId;
-    const skills = req.body.skills.map((skill) => ({
-        skill: skill.value,
-        name: skill.label,
-    }));
+  const userId = req.body.userId;
+  const skills = req.body.skills.map((skill) => ({
+    skill: skill.value,
+    name: skill.label,
+  }));
 
-    try {
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        user.isFreelancer = true;
-        user.freelancerSkills = skills;
-
-        await user.save();
-
-        return res.status(200).json(user);
-    } catch (error) {
-        console.error("Error converting user:", error);
-        return res.status(500).json({
-            message: "An error occurred while making the user a freelancer",
-        });
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    user.isFreelancer = true;
+    user.freelancerSkills = skills;
+
+    await user.save();
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error("Error converting user:", error);
+    return res.status(500).json({
+      message: "An error occurred while making the user a freelancer",
+    });
+  }
 };
